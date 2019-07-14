922a90c2aadeea3aaa4f29e7dcca64e916470a0a
josephsmann <mannalytics@gmail.com>
2019-07-14 17:22:11 -0600

    added cpdt document and accompanying files in cpdt dir

---
+++ joe/cpdt/src/.coqdeps.d
@@ -0,0 +1,44 @@
+Coinductive.vo Coinductive.glob Coinductive.v.beautified: Coinductive.v
+Coinductive.vio: Coinductive.v
+Conclusion.vo Conclusion.glob Conclusion.v.beautified: Conclusion.v
+Conclusion.vio: Conclusion.v
+CpdtTactics.vo CpdtTactics.glob CpdtTactics.v.beautified: CpdtTactics.v
+CpdtTactics.vio: CpdtTactics.v
+DataStruct.vo DataStruct.glob DataStruct.v.beautified: DataStruct.v
+DataStruct.vio: DataStruct.v
+DepList.vo DepList.glob DepList.v.beautified: DepList.v
+DepList.vio: DepList.v
+Equality.vo Equality.glob Equality.v.beautified: Equality.v
+Equality.vio: Equality.v
+Exercises.vo Exercises.glob Exercises.v.beautified: Exercises.v
+Exercises.vio: Exercises.v
+GeneralRec.vo GeneralRec.glob GeneralRec.v.beautified: GeneralRec.v
+GeneralRec.vio: GeneralRec.v
+Generic.vo Generic.glob Generic.v.beautified: Generic.v
+Generic.vio: Generic.v
+InductiveTypes.vo InductiveTypes.glob InductiveTypes.v.beautified: InductiveTypes.v
+InductiveTypes.vio: InductiveTypes.v
+Intro.vo Intro.glob Intro.v.beautified: Intro.v
+Intro.vio: Intro.v
+Large.vo Large.glob Large.v.beautified: Large.v MoreDep.vo
+Large.vio: Large.v MoreDep.vio
+LogicProg.vo LogicProg.glob LogicProg.v.beautified: LogicProg.v
+LogicProg.vio: LogicProg.v
+Match.vo Match.glob Match.v.beautified: Match.v
+Match.vio: Match.v
+MoreDep.vo MoreDep.glob MoreDep.v.beautified: MoreDep.v
+MoreDep.vio: MoreDep.v
+MoreSpecif.vo MoreSpecif.glob MoreSpecif.v.beautified: MoreSpecif.v
+MoreSpecif.vio: MoreSpecif.v
+Predicates.vo Predicates.glob Predicates.v.beautified: Predicates.v
+Predicates.vio: Predicates.v
+ProgLang.vo ProgLang.glob ProgLang.v.beautified: ProgLang.v
+ProgLang.vio: ProgLang.v
+Reflection.vo Reflection.glob Reflection.v.beautified: Reflection.v
+Reflection.vio: Reflection.v
+StackMachine.vo StackMachine.glob StackMachine.v.beautified: StackMachine.v
+StackMachine.vio: StackMachine.v
+Subset.vo Subset.glob Subset.v.beautified: Subset.v
+Subset.vio: Subset.v
+Universes.vo Universes.glob Universes.v.beautified: Universes.v DepList.vo
+Universes.vio: Universes.v DepList.vio
